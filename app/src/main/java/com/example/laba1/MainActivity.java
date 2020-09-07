28b06f896bf1eff7b3d60095a88077d7a7e71bae
artem gordeychik <dogrti100@gmail.com>
2020-09-08 00:45:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/laba1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.laba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+///TODO Add new method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
